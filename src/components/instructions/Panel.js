786bdbfdb4db43496b2a2581c670a2905ded773e
Zackhardtoname <zackL@bu.edu>
2019-06-24 21:46:22 -0400

    implemented example - not working

---
+++ src/components/instructions/Panel.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Panel = () => {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">Follow Me</h5>
+                <ol className="card-text">
+                    <li>
+                        do this
+                    </li>
+                    <li>
+                        do this
+                    </li>
+                </ol>
+            </div>
+        </div>
+    );
+}
+
+export default Panel;
