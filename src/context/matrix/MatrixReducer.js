98a8b076a627439f071838ee92a4b47fe15550f6
Zackhardtoname <zackL@bu.edu>
2019-06-28 14:50:38 -0400

    Delete unnecessary dependencies

---
+++ src/context/matrix/MatrixReducer.js
@@ -0,0 +1,14 @@
+// import {
+//     SEARCH_USERS,
+//     GET_USER,
+//     GET_REPOS,
+//     SET_LOADING,
+//     CLEAR_USERS,
+// } from "../../types"
+
+export default (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
