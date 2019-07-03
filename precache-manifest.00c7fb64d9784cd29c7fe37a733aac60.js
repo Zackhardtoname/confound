2af0df1c0ba721fa36249afe92b1436f08301ac9
Zackhardtoname <zackL@bu.edu>
2019-07-03 12:41:39 -0400

    Updates

---
+++ precache-manifest.00c7fb64d9784cd29c7fe37a733aac60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeae19a98a18a3c66e5b7cb410794a43",
+    "url": "/confound/index.html"
+  },
+  {
+    "revision": "aac2c11a64fb1af6da3f",
+    "url": "/confound/static/css/2.2d153a96.chunk.css"
+  },
+  {
+    "revision": "d543df2c0abd094b0a66",
+    "url": "/confound/static/css/main.27bee20e.chunk.css"
+  },
+  {
+    "revision": "aac2c11a64fb1af6da3f",
+    "url": "/confound/static/js/2.45735fcd.chunk.js"
+  },
+  {
+    "revision": "d543df2c0abd094b0a66",
+    "url": "/confound/static/js/main.829c3aff.chunk.js"
+  },
+  {
+    "revision": "1edbb15958b70baf773b",
+    "url": "/confound/static/js/runtime~main.9ff4f7e3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/confound/precache-manifest.8860e960a49c3fe467d63a9e83b3cdf3.js"
+  "/confound/precache-manifest.00c7fb64d9784cd29c7fe37a733aac60.js"
 );
 
 self.addEventListener('message', (event) => {
