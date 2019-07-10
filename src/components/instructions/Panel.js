7c63a50e4175e4d981cab9fefefc2638124e1e6f
Zackhardtoname <zackL@bu.edu>
2019-07-10 15:25:20 -0400

    Add some instructions

--- src/components/instructions/Panel.js
+++ src/components/instructions/Panel.js
@@ -7,13 +7,25 @@ const Panel = () => {
                 <h5 className="card-title">Follow Me</h5>
                 <ol >
                     <li>
-                        do this
+                        Edit any value via double clicks
                     </li>
                     <li>
-                        Any red cells indicate an error - click on the cell to see the error message
+                        Hover over the forest plot too see the specific values in popups
+                    </li>
+                    <li>
+                        As of right now, please input numbers in aOR
+                    </li>
+                    <li>
+                        Hover over the headers of the dropdown columns to see the name in popups
+                    </li>
+                    <li>
+                        After your changes, you can save the matrix and view it any time by uploading it
                     </li>
                 </ol>
-
+                <h5 className="card-title">Questions/Suggestions/Contributions</h5>
+                <p className="card-text tab">
+                    Please contact BU SAIL: <a href = "mailto: shreyap@bu.edu">shreyap@bu.edu</a> and <a href = "mailto: zackL@bu.edu">zackL@bu.edu</a>
+                </p>
             </div>
         </div>
     );
