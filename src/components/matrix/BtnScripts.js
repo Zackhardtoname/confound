498629332d779e04923970b323cf8c46e7ba92c1
Zackhardtoname <zackL@bu.edu>
2019-08-13 23:16:03 -0400

    Improve sorting functionality
    1. Disable native column sorting
    2. Add functionality to toggle between ascending and descending orders for our own implementation
    3. Update instructions
    
    Close #25

--- src/components/matrix/BtnScripts.js
+++ src/components/matrix/BtnScripts.js
@@ -38,26 +38,62 @@ export function importCSV(hotTableComponent) {
 }
 
 export function sortMetric() {
-    let btn = document.getElementById('sort-metric');
+    const ascBtn = document.getElementById('sort-metric');
 
-    btn.addEventListener('click', function () {
-        const metricName = window.matrixContext.metricName;
-        curData.sort(function (x, y) {
-            const x_metric = parseInput(x[1], null, null, null, false)[1];
-            const y_metric = parseInput(y[1], null, null, null, false)[1];
+    ascBtn.addEventListener('click', function () {
+        const isAscending = window.matrixContext.isAscending;
+        let compareFunction;
+        isAscending ? compareFunction = customComparisonAsc : compareFunction = customComparisonDesc;
+        curData.sort(compareFunction);
 
-            if (x_metric === y_metric) {
-                return 0
-            }
-
-            //two conditional blocks since even if x[1] or y[1] is "Metric", x_metric or y_metric could still be 1, a number
-            if (x[1] === metricName) {
-                return -1;
-            } else if (y[1] === metricName) {
-                return 1;
-            }
-
-            return x_metric > y_metric ? 1 : -1;
-        });
+        // for next time
+        window.matrixContext.toggleSorting();
     });
+}
+
+// here we have two compare functions that only differ in the last condition.
+// tried to combine them into one with the help of an external variable, but it turns out
+// from (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort)
+// "compareFunction(a, b) must always return the same value when given a specific pair of elements a and b as its two arguments. If inconsistent results are returned, then the sort order is undefined"
+
+function customComparisonAsc(x, y) {
+    const { metricName, x_metric, y_metric } = getCompareData(x, y);
+
+    //two conditional blocks since even if x[1] or y[1] is "Metric", x_metric or y_metric could still be 1, a number
+    if (x[1] === metricName) {
+        return -1;
+    } else if (y[1] === metricName) {
+        return 1;
+    }
+
+    if (x_metric === y_metric) {
+        return 0
+    }
+
+    return x_metric > y_metric ? 1 : -1;
+}
+
+function customComparisonDesc(x, y) {
+    const { metricName, x_metric, y_metric } = getCompareData(x, y);
+
+    //two conditional blocks since even if x[1] or y[1] is "Metric", x_metric or y_metric could still be 1, a number
+    if (x[1] === metricName) {
+        return -1;
+    } else if (y[1] === metricName) {
+        return 1;
+    }
+
+    if (x_metric === y_metric) {
+        return 0
+    }
+
+    return x_metric < y_metric ? 1 : -1;
+}
+
+function getCompareData(x, y) {
+    const metricName = window.matrixContext.metricName;
+    const x_metric = parseInput(x[1], null, null, null, false)[1];
+    const y_metric = parseInput(y[1], null, null, null, false)[1];
+
+    return {metricName: metricName, x_metric: x_metric, y_metric: y_metric};
 }
